401d6f3f44c83aea6a8ba9ec7020c0d91ac63c0e
Tim <tburgess83200@gmail.com>
2026-04-22 15:53:20 +0200

    add contact to footer

--- web/footer.php
+++ web/footer.php
@@ -1,3 +1,22 @@
 <footer class="site-footer">
-    <p>Construit avec HTML, CSS et PHP, et un peu d'amour. </p>
+    <div class="flavortext">
+        <p>Construit avec HTML, CSS et PHP, et un peu d'amour. </p>
+        <p>Copyright &copy; 2026 Tim Burgess--Poggioli. Tous droits reserves.</p>
+    </div>
+    <div class="spacer"></div>
+    <div class="social-links">
+        <a href="https://github.com/MetatoGithub">
+            <i class="fa fa-github"></i> GitHub
+        </a>
+        <a href="https://www.linkedin.com/in/tim-burgess-poggioli-46960b279/">
+            <i class="fa fa-linkedin"></i> LinkedIn
+        </a>
+        <a href="https://discord.com/invite/nZUW9Rgz">
+            <i class="fa-brands fa-discord"></i> Discord
+        </a>
+        <a href="mailto:tim.burgess@metatobac.cc">
+            <i class="fa fa-envelope"></i> E-mail
+        </a>
+    </div>
+
 </footer>
