8467168d34edfec64dea30801bb179aa160f3b7c
Tim <tburgess83200@gmail.com>
2026-04-25 20:56:19 +0200

    please stop caching my css cloudflare

--- web/projects.php
+++ web/projects.php
@@ -6,7 +6,7 @@
     <meta name="theme-color" content="#0a0a12">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <title>Projets | Tim Burgess--Poggioli</title>
-    <link rel="stylesheet" href="styles.css" />
+    <link rel="stylesheet" href="styles.css?newcache01" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body class="page page-projects">
